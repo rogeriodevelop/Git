294b419ad507c57dc94b84988d5e3c8917016aac
Rogerio Santos <rogeriodevelop@gmail.com>
2017-12-04 23:11:02 -0200

    While
    Novo

---
+++ Contador01/src/contador01/Contador01.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contador01;
+
+/**
+ *
+ * @author Black
+ */
+public class Contador01 {
+
+    /**
+     * @param args the command line arguments
+     */
+        // TODO code application logic here
+         int cc = 0;
+        
+        while (cc<4) {
+            System.out.println("Cambalhota");
+            cc++;
+        }
+    }
+    
+}
