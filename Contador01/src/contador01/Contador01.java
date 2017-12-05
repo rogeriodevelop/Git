1b5f4ad1ed11e0ce35be0dcf411c847a31c244fe
Rogerio Santos <rogeriodevelop@gmail.com>
2017-12-04 23:19:51 -0200

    Contador mostrando o numero

--- Contador01/src/contador01/Contador01.java
+++ Contador01/src/contador01/Contador01.java
@@ -14,11 +14,12 @@ public class Contador01 {
     /**
      * @param args the command line arguments
      */
+    public static void main(String[] args) {
         // TODO code application logic here
          int cc = 0;
         
         while (cc<4) {
-            System.out.println("Cambalhota");
+            System.out.println("Cambalhota" + cc);
             cc++;
         }
     }
