76d74936bc82a08a8db840a9c1379730b3f11a95
Rogerio Santos <rogeriodevelop@gmail.com>
2018-01-05 01:10:59 -0200

    Metodos e atributos

---
+++ Aula02/src/aula02/Caneta.java
@@ -0,0 +1,35 @@
+
+package aula02;
+
+public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+    
+    void status(){
+        System.out.println("Modelo " + this.modelo);
+        System.out.println("Uma caneta " + this.cor);
+        System.out.println("Ponta " + this.ponta);
+        System.out.println("Carga " + this.carga);
+        System.out.println("Está tampada? " + this.tampada);
+    }
+    void rabiscar(){
+        if (this.tampada == true){
+            System.out.println("Erro não posso rabiscar a caneta está tampada");
+        }else{
+            System.out.println("Estou rabsiscando");
+        }
+        
+    }
+    
+    void tampar(){
+        this.tampada = true;
+    }
+    
+    void destampar(){
+        this.tampada = false;
+    }
+    
+}
---
+++ RepeticaoFor/src/repeticaofor/RepeticaoFor.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repeticaofor;
+
+/**
+ *
+ * @author Black
+ */
+public class RepeticaoFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        for (int c=0;c < 4;c++){
+            System.out.println("Cambalhota"+ c );
+        }
+    }
+    
+}
